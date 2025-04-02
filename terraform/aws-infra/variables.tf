e9cea97c6846c5d36c7768d2c2da4a719e15b1d8
sanket daule <sanketdaule25@gmail.com>
2025-04-02 19:40:08 +0530

    variables updated

--- terraform/aws-infra/variables.tf
+++ terraform/aws-infra/variables.tf
@@ -4,16 +4,6 @@ variable "region" {
   default     = "ap-south-1"
 }
 
-variable "ami" {
-  description = "ami of the ec2"
-  type        = string
-}
-
-variable "instance_type" {
-  description = "instance type of ec2"
-  type        = string
-
-}
 variable "environment" {
   type = string
 }
